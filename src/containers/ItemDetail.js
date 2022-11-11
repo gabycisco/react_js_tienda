9aec01b783c345e75d54d715dad48b6c88ff1f14
Gaby Tomasiello <gaby.cisco@gmail.com>
2022-11-10 23:29:42 -0300

    Se ajusta estilo de modulo de detalle

--- src/containers/ItemDetail.js
+++ src/containers/ItemDetail.js
@@ -4,18 +4,26 @@ const ItemDetail = ({ item }) => {
     } */
     return (
       <div className="detail-product">
-          <span className="product-detail-title">{item.title}</span>
+          
           <div className="principal-container">
             <div className="detail-product-image">
-            <img src={item.pictureUrl} alt="Product" />
+              <img src={item.pictureUrl} alt="Product" />
             </div>
             <div className="product-details">
-            <p>{item.description}</p>
-            <p>Stock: {item.stock}</p> 
-            <h5>${item.price}</h5>
+            <span className="product-detail-title">{item.title}</span>
+              <p>{item.description}</p>
+              <p>Stock: {item.stock}</p> 
+              <h5>${item.price}</h5>
+
+              <div className="cantContainer">
+                <button>-</button>
+                <div className="cantidad">1</div>
+                <button>+</button>
+              </div>
+              <button className="detailsItem-bt" onClick={item.buyItem}>Agregar al carrito</button>
             </div>
         </div>
-          <button className="buyItem-bt" onClick={item.buyItem}>SUMAR AL CARRITO</button>
+          
     </div>
     )
   }
